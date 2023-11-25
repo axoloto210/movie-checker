4f7619e111f92628631112264fb04bdbb1ca1fb4
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2023-11-25 21:05:18 +0900

    next configでimages domainの使用が非推奨となっていたため修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com']
+    //Next 14から非推奨に。
+    // domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      }
+    ]
   }
 }
 
