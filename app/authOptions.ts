74b98f6b524e33eb08e40a0775d25587c861704a
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2023-11-25 20:18:00 +0900

    NextAuth.jsでlocalhostのGoogle認証とGithub認証を追加

---
+++ app/authOptions.ts
@@ -0,0 +1,28 @@
+import type { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions: NextAuthOptions = {
+  //認証情報をconsole上で確認したい場合にはtrueに設定。
+  //debug: true,
+
+  //session: { strategy: 'jwt' },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code'
+        }
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+}
---
+++ app/components/LoginButton.tsx
@@ -0,0 +1,21 @@
+import { signOut, signIn, useSession } from 'next-auth/react'
+
+export default function Login() {
+  const { data: session } = useSession()
+
+  if (session) {
+    return (
+      <>
+        <div>
+          <button onClick={() => signOut()}>ログアウト</button>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <div>
+        <button onClick={() => signIn()}>ログイン</button>
+      </div>
+    )
+  }
+}
