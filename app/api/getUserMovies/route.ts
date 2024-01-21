98a122d92847699664b86585eb20645f8f8e4b02
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-01-22 00:52:58 +0900

    登録した映画情報を表示できるページの基盤を作成

---
+++ app/api/getUserMovies/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userEmail } = await req.json()
+    if (userEmail === undefined || userEmail === null) {
+      throw new Error()
+    }
+
+    const user = await getUserId(userEmail)
+
+    if (user !== null) {
+      const userMovies = await getUserMovies(user.id)
+      return Response.json({ userMovies, status: 200 })
+    }
+  } catch (error: unknown) {
+    console.log(error)
+    return Response.json({ status: 500 })
+  }
+}
+
+const prisma = new PrismaClient()
+
+async function getUserId(userEmail: string) {
+  const userId = await prisma.user.findUnique({
+    where: {
+      email: userEmail
+    },
+    select: {
+      id: true
+    }
+  })
+  return userId
+}
+
+async function getUserMovies(userId: string) {
+  const userMovies = await prisma.movie.findMany({
+    where: {
+      authorId: userId
+    },
+    select: {
+      title: true,
+      siteURL: true,
+      image: true
+    }
+  })
+  return userMovies
+}
---
+++ app/mypage/movie/page.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import { NextAuthProvider } from '../../providers'
+import ResponsiveAppBar from '../../components/Header'
+import { Fragment, useEffect, useState } from 'react'
+import { RegisteredMovie } from '@/app/components/feature/movieRegistration/MovieRegistrationForm'
+
+export default function MyMoviepage() {
+  return (
+    <NextAuthProvider>
+      <ClientMyMovie />
+    </NextAuthProvider>
+  )
+}
+
+function ClientMyMovie() {
+  const { data: session } = useSession()
+  const user = session?.user
+
+  const [movies, setMovies] = useState<RegisteredMovie[]>([])
+
+  //getServerSidePropsの方が簡潔。
+  useEffect(() => {
+    if (user?.email !== undefined) {
+      const fetchMovies = async () => {
+        const url = process.env.NEXT_PUBLIC_BASE_URL + '/api/getUserMovies'
+
+        const params = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            userEmail: user?.email
+          })
+        }
+
+        await fetch(url, params)
+          .then((res) => res.json())
+          .then((res) => {
+            const { userMovies } = res
+            setMovies(userMovies)
+          })
+          .catch(() =>
+            alert('エラーが発生しました。時間をあけて再度お試しください。')
+          )
+      }
+      fetchMovies()
+    }
+  }, [user?.email])
+
+  return (
+    <>
+      <ResponsiveAppBar userImage={user?.image ?? undefined} />
+      {session && (
+        <>
+          <h2 className="flex justify-center items-center">登録済み映画</h2>
+          <table>
+            <tbody>
+              {movies?.map((movie) => {
+                return (
+                  <Fragment key={movie.title}>
+                    <tr>
+                      <th>タイトル</th>
+                      <td>{movie.title}</td>
+                    </tr>
+                    <tr>
+                      <th>サイトURL</th>
+                      <td>
+                        <a href={movie.siteURL}>{movie.siteURL}</a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>画像</th>
+                      <td>
+                        <img src={movie.image} />
+                      </td>
+                    </tr>
+                  </Fragment>
+                )
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
+  )
+}
