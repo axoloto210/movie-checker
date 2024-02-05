66e6763f0a940ce66447bda5c653a2576582f9d9
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-02-06 03:15:44 +0900

    dnd-kitを導入し、並べ替え機能を追加

--- app/mymovie/page.tsx
+++ app/mymovie/page.tsx
@@ -2,8 +2,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '../api/auth/authOptions'
 import { notFound } from 'next/navigation'
 import prisma from '../lib/prisma'
-import { Fragment } from 'react'
-import Image from 'next/image'
+import { MovieDnDList } from '../features/mymovie/MovieDnDList'
 
 export default async function MyMovie() {
   const session = await getServerSession(authOptions)
@@ -29,49 +28,13 @@ export default async function MyMovie() {
     }
   })
   //TODO: 画像の縮尺を維持する
+
   return (
     <>
       {session && (
         <>
           <h2 className="flex justify-center items-center">登録済み映画</h2>
-          <table>
-            <tbody>
-              {movies?.map((movie) => {
-                return (
-                  <Fragment key={movie.title}>
-                    <tr>
-                      <th>{}</th>
-                      <td>
-                        <Image
-                          src={movie.image ?? ''}
-                          alt={movie.title}
-                          width={120}
-                          height={180}
-                          unoptimized={true}
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>{}</th>
-                      <td>{movie.title}</td>
-                    </tr>
-                    <tr>
-                      <th>サイトURL</th>
-                      <td>
-                        <a
-                          href={movie.siteURL ?? '/'}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {movie.siteURL}{' '}
-                        </a>
-                      </td>
-                    </tr>
-                  </Fragment>
-                )
-              })}
-            </tbody>
-          </table>
+          <MovieDnDList movies={movies} />
         </>
       )}
     </>
