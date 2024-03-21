dcf0f898f5be8bbacba5988c82e6611bd3ba3f7e
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-03-21 19:03:21 +0900

    OGPの設定追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,22 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const siteName = 'Movie Checker'
+const description =
+  'Find your movie you watched or you want to watch. みた映画・みたい映画を管理しよう。'
+const url = 'https://movie-checker-omega.vercel.app'
+
 export const metadata: Metadata = {
-  title: 'Movie Checker',
-  description: 'Find your movie you watched or you want to watch.'
+  title: siteName,
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website'
+  }
 }
 
 export default function RootLayout({
