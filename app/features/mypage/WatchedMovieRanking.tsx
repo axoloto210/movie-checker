1550064b4ba5902c20a3943bdf77c1974befeaec
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-05-08 00:09:25 +0900

    マイページで上位3件の「みた映画」を取得

--- app/features/mypage/WatchedMovieRanking.tsx
+++ app/features/mypage/WatchedMovieRanking.tsx
@@ -1,7 +1,47 @@
-export function WatchedMovieRanking() {
+import { authOptions } from '@/api/auth/authOptions'
+import prisma from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { Fragment } from 'react'
+
+export async function WatchedMovieRanking() {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/mypage')
+  }
+  const author = await prisma.user.findFirst({
+    where: { email: session.user?.email },
+    select: { id: true }
+  })
+
+  if (author === null) {
+    redirect('/mypage')
+  }
+  const movies = await prisma.movie.findMany({
+    where: {
+      authorId: author.id
+    },
+    select: {
+      id: true,
+      title: true,
+      siteURL: true,
+      image: true,
+      order: true
+    },
+    orderBy: {
+      order: 'asc'
+    },
+    take: 3
+  })
   return (
     <>
-      <div>hi</div>
+      {movies.map((movie) => {
+        return (
+          <Fragment key={movie.id}>
+            <div>{movie.title}</div>
+          </Fragment>
+        )
+      })}
     </>
   )
 }
