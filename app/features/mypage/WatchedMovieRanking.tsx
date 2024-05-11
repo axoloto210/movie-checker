45c1f6c130543398563ec6f8d6188c826de1ab61
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-05-11 20:10:51 +0900

    みた映画TOP3のデザイン追加

--- app/features/mypage/WatchedMovieRanking.tsx
+++ app/features/mypage/WatchedMovieRanking.tsx
@@ -3,8 +3,10 @@ import prisma from '@/lib/prisma'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { Fragment } from 'react'
-import '../mypage/watchedMovieRanking.css'
+import styles from '@/features/mypage/watchedMovieRanking.module.scss'
 import Image from 'next/image'
+
+const rankLimit = 3 as const
 
 export async function WatchedMovieRanking() {
   const session = await getServerSession(authOptions)
@@ -33,21 +35,18 @@ export async function WatchedMovieRanking() {
     orderBy: {
       order: 'asc'
     },
-    take: 3
+    take: rankLimit
   })
   return (
     <>
-      <section>
-        <div className="container">
-          {movies.map((movie, index) => {
-            return (
-              <Fragment key={movie.id}>
-                {MovieRankingCard(movie, index)}
-              </Fragment>
-            )
-          })}
-        </div>
-      </section>
+      <div className={styles.containerTitle}>{`みた映画 TOP${rankLimit}`}</div>
+      <div className={styles.container}>
+        {movies?.map((movie, index) => {
+          return (
+            <Fragment key={movie.id}>{MovieRankingCard(movie, index)}</Fragment>
+          )
+        })}
+      </div>
     </>
   )
 }
@@ -67,18 +66,23 @@ const MovieRankingCard = (
       <p>
         <span>{index + 1}</span>
       </p>
-      <div>
+      <a
+        className={styles.link}
+        href={movie.siteURL ?? '/mymovie'}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
-          className="img_container"
+          className={styles.imgContainer}
           src={movie.image ?? '/favicon.ico'}
           alt="movie image"
           width="100"
           height="100"
           unoptimized
         />
-      </div>
-      <div className="">
-        <p className="">{movie.title}</p>
+      </a>
+      <div className={styles.movieLabel}>
+        <p className={styles.movieTitle}>{movie.title}</p>
       </div>
     </div>
   )
