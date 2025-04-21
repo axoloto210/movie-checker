395b96bdcfde339cec7520d1a15585a73df0bebb
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-21 22:32:19 +0900

    コンポーネントの切り出し

--- app/features/mypage/WatchListRanking.tsx
+++ app/features/mypage/WatchListRanking.tsx
@@ -4,8 +4,7 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { Fragment } from 'react'
 import styles from '@/features/mypage/watchedMovieRanking.module.scss'
-import Image from 'next/image'
-import { RankIcon } from '../../components/RankIcon'
+import { MovieRankingCard } from './MovieRankingCard'
 
 const rankLimit = 3 as const
 
@@ -53,43 +52,5 @@ export async function WatchListRanking() {
         })}
       </div>
     </>
-  )
-}
-
-const MovieRankingCard = (
-  movie: {
-    id: number
-    image: string | null
-    title: string
-    siteURL: string | null
-    order: number
-  },
-  index: number
-) => {
-  return (
-    <div>
-      <p className={styles.rank}>
-        <RankIcon order={index + 1} width="20px" height="20px" />
-        <span className="ml-1 mt-1">{index + 1} </span>
-      </p>
-      <a
-        className={styles.link}
-        href={movie.siteURL ?? '/mymovie'}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.imgContainer}
-          src={movie.image ?? '/favicon.ico'}
-          alt="movie image"
-          width="100"
-          height="100"
-          unoptimized
-        />
-      </a>
-      <div className={styles.movieLabel}>
-        <p className={styles.movieTitle}>{movie.title}</p>
-      </div>
-    </div>
   )
 }
