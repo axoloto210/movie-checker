17ed2c8450418dea4fd98529eb22f962f71699fe
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-05-27 08:17:25 +0900

    非推奨となった方を削除

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { type ElementRef, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import { createPortal } from 'react-dom'
 
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter()
-  const dialogRef = useRef<ElementRef<'dialog'>>(null)
+  const dialogRef = useRef<HTMLDialogElement>(null)
 
   useEffect(() => {
     if (!dialogRef.current?.open) {
